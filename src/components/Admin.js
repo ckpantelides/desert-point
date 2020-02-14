9e7ee7857422d0944a3d6500d9be257f7884d476
CK Pantelides <ckpantelides@gmail.com>
2020-02-14 15:37:17 +0000

    Added ability to drag and drop enquiries in admin

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,6 +1,10 @@
-import React from "react";
+//import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
+
+import React from "react";
+import { SortableContainer, SortableElement } from "react-sortable-hoc";
+import arrayMove from "array-move";
 
 const Admin = () => {
   // const url = "https://jsonplaceholder.typicode.com/users";
@@ -11,7 +15,55 @@ const Admin = () => {
     axios.get(url).then(json => setData(json.data));
   }, []);
 
-  const renderTable = () => {
+  const SortableEnquiry = SortableElement(({ value }) => (
+    <div className="enquiry-card">
+      <p>{value.name}</p>
+      <p>{value.email}</p>
+      <p>{value.message}</p>
+    </div>
+  ));
+
+  const SortableEnquiryContainer = SortableContainer(({ items }) => (
+    <div className="enquiry-card-container">
+      {items.map((value, index) => (
+        <SortableEnquiry key={`item-${value}`} index={index} value={value} />
+      ))}
+    </div>
+  ));
+
+  const onSortEnd = ({ oldIndex, newIndex }) =>
+    setData(arrayMove(data, oldIndex, newIndex));
+
+  /*
+  return (
+    <div id="admin">
+      <h1>Admin</h1>
+      <SortableEnquiryContainer axis="xy" onSortEnd={onSortEnd} items={data}>
+        {data.map((user, i) => (
+          <SortableEnquiry />
+        ))}
+      </SortableEnquiryContainer>
+    </div>
+  );
+  */
+  return (
+    <div id="admin">
+      <h1>Admin</h1>
+      <SortableEnquiryContainer
+        axis="xy"
+        onSortEnd={onSortEnd}
+        items={data}
+        onSortStart={(_, event) => event.preventDefault()}
+        pressDelay="100"
+      />
+    </div>
+  );
+};
+
+export default Admin;
+
+/*
+  const renderEnquiries = () => {
     return data.map(user => {
       return (
         <div className="enquiry-card">
@@ -27,10 +79,11 @@ const Admin = () => {
     <section id="admin">
       <div>
         <h1 id="title">Admin</h1>
-        <div className="enquiry-card-container">{renderTable()}</div>
+        <div className="enquiry-card-container">{renderEnquiries()}</div>
       </div>
     </section>
   );
 };
 
 export default Admin;
+*/
