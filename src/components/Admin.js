cd56fe6214a47a2a4f96595a68e8739c94853395
CK Pantelides <ckpantelides@gmail.com>
2020-02-17 17:51:01 +0000

    Added functions to 'mark read' and delete enquiries in Admin

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -15,18 +15,57 @@ const Admin = () => {
     axios.get(url).then(json => setData(json.data));
   }, []);
 
-  const SortableEnquiry = SortableElement(({ value }) => (
-    <div className="enquiry-card">
-      <p>{value.name}</p>
-      <p>{value.email}</p>
-      <p>{value.message}</p>
-    </div>
-  ));
+  // function to mark enquiries as "read". All enquiries are initially set as read:"false".
+  // This function creates a copy of the data retrieved from the database. When the rowid matches
+  // the rowid of the data, read is marked as "true"
+  function readEnquiry(rowid) {
+    const updatedData = data.map(data =>
+      // read: "true" rather than true as sqlite does not accept booleans
+      // will be updated for release with postgressql
+      data.rowid === rowid ? { ...data, read: "true" } : data
+    );
+    setData(updatedData);
+  }
+
+  function deleteEnquiry(rowid) {
+    const filteredData = data.filter(data => data.rowid !== rowid);
+    setData(filteredData);
+  }
+
+  const SortableEnquiry = SortableElement(
+    ({ value, readEnquiry, deleteEnquiry }) => (
+      // currently === "true" not true as sqlite not accept boolean
+      <div className={`enquiry-card${value.read === "true" ? " read" : ""}`}>
+        <span className="enquiryButtonWrapper">
+          <button className="enquiryButton" type="button" onClick={readEnquiry}>
+            <img src="images/tick.png" className="buttonIcon" alt="tick" />
+          </button>
+          <button
+            className="enquiryButton"
+            type="button"
+            onClick={deleteEnquiry}
+          >
+            <img src="images/bin.png" className="buttonIcon" alt="bin" />
+          </button>
+        </span>
+        <p>{value.name}</p>
+        <p>{value.email}</p>
+        <p>{value.message}</p>
+      </div>
+    )
+  );
 
   const SortableEnquiryContainer = SortableContainer(({ items }) => (
     <div className="enquiry-card-container">
       {items.map((value, index) => (
-        <SortableEnquiry key={`item-${value}`} index={index} value={value} />
+        // <SortableEnquiry key={`item-${value}`} index={index} value={value} />
+        <SortableEnquiry
+          key={value.rowid}
+          index={index}
+          value={value}
+          readEnquiry={() => readEnquiry(value.rowid)}
+          deleteEnquiry={() => deleteEnquiry(value.rowid)}
+        />
       ))}
     </div>
   ));
@@ -34,21 +73,13 @@ const Admin = () => {
   const onSortEnd = ({ oldIndex, newIndex }) =>
     setData(arrayMove(data, oldIndex, newIndex));
 
-  /*
   return (
     <div id="admin">
-      <h1>Admin</h1>
-      <SortableEnquiryContainer axis="xy" onSortEnd={onSortEnd} items={data}>
-        {data.map((user, i) => (
-          <SortableEnquiry />
-        ))}
-      </SortableEnquiryContainer>
-    </div>
-  );
-  */
-  return (
-    <div id="admin">
-      <h1>Admin</h1>
+      <h1>Enquiries</h1>
+      <h4>
+        You can drag the enquiries to reorder them, mark them as "read" or
+        delete them
+      </h4>
       <SortableEnquiryContainer
         axis="xy"
         onSortEnd={onSortEnd}
