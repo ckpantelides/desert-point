b6718c2c2e2a6d203b74f9afd3f310eed4106c4c
CK Pantelides <ckpantelides@gmail.com>
2020-02-21 21:22:38 +0000

    App switched to Component class

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import NavBackground from './components/NavBackground';
 import Header from './components/Header';
 import About from './components/About';
@@ -10,24 +10,26 @@ import './App.css';
 
 import { HashRouter, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <HashRouter basename='/'>
-      <div className='App'>
-        <Route exact path='/'>
-          <NavBackground />
-          <Header />
-          <About />
-          <Packages />
-          <Booking />
-        </Route>
-        <Route path='/about' component={About} />
-        <Route path='/packages' component={Packages} />
-        <Route path='/booking' component={Booking} />
-        <Route path='/admin' component={Admin} />
-      </div>
-    </HashRouter>
-  );
+class App extends Component {
+  render() {
+    return (
+      <HashRouter basename='/'>
+        <div className='App'>
+          <Route exact path='/'>
+            <NavBackground />
+            <Header />
+            <About />
+            <Packages />
+            <Booking />
+          </Route>
+          <Route path='/about' component={About} />
+          <Route path='/packages' component={Packages} />
+          <Route path='/booking' component={Booking} />
+          <Route path='/admin' component={Admin} />
+        </div>
+      </HashRouter>
+    );
+  }
 }
 
 export default App;
