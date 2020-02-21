c0499d9e41944cc1dd1e4651160e11a1b8fdbbd6
CK Pantelides <ckpantelides@gmail.com>
2020-02-21 21:01:31 +0000

    Updates

---
+++ precache-manifest.1d8d7702ce93d6856e438e3308997d78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e4bfd990d6f066a6e7824b682d640e9",
+    "url": "/desert-point/index.html"
+  },
+  {
+    "revision": "c2143587d3bb088f2e17",
+    "url": "/desert-point/static/css/main.a4f58af4.chunk.css"
+  },
+  {
+    "revision": "6ceafc326e431c200135",
+    "url": "/desert-point/static/js/2.642ba590.chunk.js"
+  },
+  {
+    "revision": "c2143587d3bb088f2e17",
+    "url": "/desert-point/static/js/main.9cd3f584.chunk.js"
+  },
+  {
+    "revision": "727ea9bc2ba3f5ae1fbb",
+    "url": "/desert-point/static/js/runtime-main.7b1058c7.js"
+  },
+  {
+    "revision": "b5e60ade9e146b06da80fe18212ff374",
+    "url": "/desert-point/static/media/homepage1.b5e60ade.jpg"
+  }
+]);
